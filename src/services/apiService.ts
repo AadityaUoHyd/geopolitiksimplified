3ec151a3d5e059dad5fc08a71b8346f79086e25e
AadityaUoHyd <aadiraj48@gmail.com>
2025-04-08 17:00:19 +0530

    git ignore added commit

--- src/services/apiService.ts
+++ src/services/apiService.ts
@@ -74,7 +74,7 @@ class ApiService {
 
   private constructor() {
     this.api = axios.create({
-      baseURL: '/api/v1',
+      baseURL: import.meta.env.VITE_BACKEND_URL || '/api/v1',
       headers: {
         'Content-Type': 'application/json'
       }
